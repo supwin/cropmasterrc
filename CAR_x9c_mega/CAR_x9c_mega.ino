b8ca3fadefb687575a3fba6c0af5321f2ade3867
Suparoek Wina <supwin@users.noreply.github.com>
2023-05-10 13:40:26 +0700

    dev 0.03
    ปรับโค้ดให้สต๊าสออกมาเป็น function และใช้ while ไม่ให้วน loop function  (ไม่ได้ทดสอบจริง ทดสอบในจินตนาการเท่านั้น)
    
    ปรับการขับเคลื่อนรถ ให้เป็น
    *เดินหน้า
    *ถอยหลัง
    *หมุนซ้าย
    *หมุนขวา
    *ถอยซ้าย
    *ถอยขวา
    เพื่อให้รองรับการใช้งานแบบ auto pilot  (ไม่ได้ทดสอบจริง ทดสอบในจินตนาการเท่านั้น)
    
    เพิ่ม โครงสำหรับ gps  เตรียมการเพื่อทำแปลนและ waypoint

--- CAR_x9c_mega/CAR_x9c_mega.ino
+++ CAR_x9c_mega/CAR_x9c_mega.ino
@@ -1,8 +1,13 @@
 #include <SD.h>         // Include the SD library
 #include <TMRpcm.h>     // Include the TMRpcm library
 #include <IBusBM.h>   
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
 IBusBM ibus;
-TMRpcm audio;           // Create an object of TMRpcm class
+TMRpcm audio; // Create an object of TMRpcm class
+TinyGPSPlus gps;  // The TinyGPS++ object
+SoftwareSerial ss(RXPin, TXPin); // The serial connection to the GPS device
 
 #define rev_L 5
 #define rev_R 6
@@ -11,20 +16,15 @@ TMRpcm audio;           // Create an object of TMRpcm class
 /*
 #define gearGND 50
 #define gearSW 52
-
 #define keyStartPIN 31*/
 #define startBladePIN 7
 
-int ch_L_Value;
-int ch_L_Val;
-uint8_t r_L_Value;
-int ch_R_Value;
-int ch_R_Val;
-uint8_t r_R_Value;
-bool startMotorControllerBox=false; //เก็บไว้ยังไม่ได้ใช้ อาจจะเอาออก
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+int ch_1;
+int ch_2;
 int str0,str1,str2,str3;
 bool bladeStatusON=false;
-bool greetingPlaied=false; 
 bool startEngine=true;
 
 int readChannel(byte channelInput, int minLimit, int maxLimit, int defaultValue) {
@@ -41,6 +41,7 @@ bool readSwitch(byte channelInput, bool defaultValue) {
 
 void setup() {
   Serial.begin(9600);
+  ss.begin(GPSBaud);
   ibus.begin(Serial1);
   Serial.println("Starting...");  
 
@@ -64,7 +65,6 @@ void setup() {
   //digitalWrite(gearGND,HIGH);
   //digitalWrite(gearSW,HIGH);
 
-  
   audio.speakerPin = 46; // Set the output pin for audio  สำหรับ mega ต้องขา 46 จึงจะมีเสียง
   audio.setVolume(5);   // Set the volume to a value between 0 and 7  ต้องกำหนดที่ 5 เท่านั้นจึงจะมีเสียงอาจจะเป็นที่สเปคลำโพง
   if (!SD.begin(53)) {  // see if the card is present and can be initialized:
@@ -72,11 +72,11 @@ void setup() {
     return;   // don't do anything more if not
   }
   wavPlay("greeting.wav",true,"Crop Master RC รถบังคับเพื่องานการเกษตร สวัสดีครับ");
-  readSubVersion("0p020000p1");
+  readSubVersion("0p03");
 }
 
 void readSubVersion(char* txt){
-  wavPlay("alfavrsn.wav",true,"อัลฟ่าเวอร์ชั่น");
+  wavPlay("devvrsn.wav",true,"เดเวลอปเวอร์ชั่น");
   for(int i = 0; i < strlen(txt); i++) {
     char txtchar = txt[i];
     String filename = String(txtchar) + ".wav";
@@ -134,110 +134,167 @@ void cancelBladStart(){
     wavPlay("ccblstat.wav",true,"ยกเลิก ใบตัด");
 }
 
-
-void loop(){
-  startEngine=true;
-  if(!startMotorControllerBox){
-    if(!readSwitch(5, false) && !readSwitch(6, false)){  //vrB ถ้าไม่เปิดใบตัดไว้(flase) หรือ swA ถ้าไม่ปิดฉุกเฉิน(flase)ไว้
-      if(startOrder()){
-        startEngine=false;  
-        wavPlay("3.wav",false,"3");
-        delay(1000);
-        if(startOrder()){
-          wavPlay("2.wav",false,"2");
-          delay(1000);
-          if(startOrder()){
-            wavPlay("1.wav",false,"1");
-            delay(1000);
-            wavPlay("carready.wav",true,"รถพร้อมทำงาน และกำลังเคลื่อนตัว กรุณาถอยออกห่าง");
-            Serial.println("Ready....");
-            startMotorControllerBox=true; //เก็บไว้ยังไม่ได้ใช้ อาจจะเอาออก
-            startEngine=true;  //สถานะพร้อมรับคำสั่ง อันตรายถ้าอยู่ใกล้
-            //digitalWrite(keyStartPIN,LOW);
-          }
-        }
-      }
-      if(!startEngine){
-        wavPlay("ccelst.wav",true,"ยกเลิกการสต๊าส");
-      }
-    }else{
-      if(readSwitch(5, false)) wavPlay("blondstr.wav",true,"สวิทช์ใบตัดเปิดค้างอยู่ ห้ามสต๊าส");
-      if(readSwitch(6, false)) wavPlay("enofdstr.wav",true,"สวิทช์เครื่องปิดอยู่ ไม่สามารถสต๊าสได้");
-    }
-  }else{
-    // หยุดเครื่องฉุกเฉินจะต้อง upgrade เป็นเคส interup ภายหลัง
-    if(readSwitch(6,false)){ //ถ้าสถานะ sw7 เป็น true 
-      emergencyStop();
-      return;  // ออกไปเริ่ม loop ใหม่ ไม่ทำคำสั่งที่เหลือต่อไป
-    }
-
-    if(readSwitch(5, false) && !bladeStatusON && !readSwitch(6,false)){  //เปิดสวิทช์ และใบตัดไม่ได้ทำงานอยู่ และ swA ต้อง true ด้วย
-      wavPlay("blstrt.wav",true,"ใบตัดกำลังเริ่มทำงานใน"); 
+bool startSystem(){
+  if(!readSwitch(5, false) && !readSwitch(6, false)){  //vrB ถ้าไม่เปิดใบตัดไว้(flase) หรือ swA ถ้าไม่ปิดฉุกเฉิน(flase)ไว้
+    if(startOrder()){ 
       wavPlay("3.wav",false,"3");
       delay(1000);
-      if(!readSwitch(6, false)){
-        if(readSwitch(5,false)){
-          wavPlay("2.wav",false,"2");
-          delay(1000);
-        }else{
-          cancelBladStart();
-          return; // ออกไปเริ่ม loop ใหม่ ไม่ทำคำสั่งที่เหลือต่อไป
-        }
-      }else{
-        emergencyStop();
-        return; // ออกไปเริ่ม loop ใหม่ ไม่ทำคำสั่งที่เหลือต่อไป
-      }
-      
-      if(!readSwitch(6, false)){
-        if(readSwitch(5,false)){
+      if(startOrder()){
+        wavPlay("2.wav",false,"2");
+        delay(1000);
+        if(startOrder()){
           wavPlay("1.wav",false,"1");
           delay(1000);
-        }else{
-          cancelBladStart();
-          return; // ออกไปเริ่ม loop ใหม่ ไม่ทำคำสั่งที่เหลือต่อไป
+          wavPlay("carready.wav",true,"รถพร้อมทำงาน และกำลังเคลื่อนตัว กรุณาถอยออกห่าง");
+          Serial.println("Ready....");
+          startMotorControllerBox=true; //เก็บไว้ยังไม่ได้ใช้ อาจจะเอาออก
+          return true;  //สถานะพร้อมรับคำสั่ง อันตรายถ้าอยู่ใกล้
+          //digitalWrite(keyStartPIN,LOW);
         }
+      }
+    }
+    wavPlay("ccelst.wav",true,"ยกเลิกการสต๊าส");
+  }else{
+    if(readSwitch(5, false)) wavPlay("blondstr.wav",true,"สวิทช์ใบตัดเปิดค้างอยู่ ห้ามสต๊าส");
+    if(readSwitch(6, false)) wavPlay("enofdstr.wav",true,"สวิทช์เครื่องปิดอยู่ ไม่สามารถสต๊าสได้");
+  }
+  return false;
+}
+
+void loop(){
+
+  while(!startEngine){  // รอการสต๊าส
+    startEngine=startSystem();
+  }
+  
+  // หยุดเครื่องฉุกเฉินจะต้อง upgrade เป็นเคส interup ภายหลัง
+  if(readSwitch(6,false)){ //ถ้าสถานะ sw7 เป็น true 
+    emergencyStop();
+    return;  // ออกไปเริ่ม loop ใหม่ ไม่ทำคำสั่งที่เหลือต่อไป
+  }
+
+  if(readSwitch(5, false) && !bladeStatusON && !readSwitch(6,false)){  //เปิดสวิทช์ และใบตัดไม่ได้ทำงานอยู่ และ swA ต้อง true ด้วย
+    wavPlay("blstrt.wav",true,"ใบตัดกำลังเริ่มทำงานใน"); 
+    wavPlay("3.wav",false,"3");
+    delay(1000);
+    if(!readSwitch(6, false)){
+      if(readSwitch(5,false)){
+        wavPlay("2.wav",false,"2");
+        delay(1000);
       }else{
-        emergencyStop();
+        cancelBladStart();
         return; // ออกไปเริ่ม loop ใหม่ ไม่ทำคำสั่งที่เหลือต่อไป
       }
-      
-      digitalWrite(startBladePIN,LOW);
-      Serial.println("bladeStared");
-      bladeStatusON = true;
-    }else if(!readSwitch(5, false) && bladeStatusON){  //ปิดสวิทช์ และใบตัดทำงานอยู่
-      digitalWrite(startBladePIN,HIGH);
-      Serial.println("bladeStoped");
-      bladeStatusON = false;
-      wavPlay("bldstop.wav",false,"ใบตัดหยุดทำงาน"); //ใบตัดหยุดทำงาน 
-    }
-        
-
-    ch_L_Value = readChannel(0, -60, 60, 0);
-    ch_R_Value = readChannel(1, -60, 60, 0);
-
-    
-    if(ch_L_Value<0){
-      ch_L_Value = ch_L_Value*(-1);    
-      digitalWrite(rev_L,LOW);
-      Serial.print("L Backward "); 
     }else{
-      digitalWrite(rev_L,HIGH);
-      Serial.print("L Forward ");  
+      emergencyStop();
+      return; // ออกไปเริ่ม loop ใหม่ ไม่ทำคำสั่งที่เหลือต่อไป
     }
-    Serial.println(ch_L_Value);
-    analogWrite(pin_L,map(ch_L_Value,0,60,75,130));
     
-    if(ch_R_Value<0){
-      ch_R_Value = ch_R_Value*(-1);  
-      digitalWrite(rev_R,LOW);
-      Serial.print("R Backward ");
+    if(!readSwitch(6, false)){
+      if(readSwitch(5,false)){
+        wavPlay("1.wav",false,"1");
+        delay(1000);
+      }else{
+        cancelBladStart();
+        return; // ออกไปเริ่ม loop ใหม่ ไม่ทำคำสั่งที่เหลือต่อไป
+      }
     }else{
-      digitalWrite(rev_R,HIGH);
-      Serial.print("R Forward ");
+      emergencyStop();
+      return; // ออกไปเริ่ม loop ใหม่ ไม่ทำคำสั่งที่เหลือต่อไป
     }
-    Serial.println(ch_R_Value);
-    analogWrite(pin_R,map(ch_R_Value,0,60,75,130));
     
-    delay(250);
+    digitalWrite(startBladePIN,LOW);
+    Serial.println("bladeStared");
+    bladeStatusON = true;
+  }else if(!readSwitch(5, false) && bladeStatusON){  //ปิดสวิทช์ และใบตัดทำงานอยู่
+    digitalWrite(startBladePIN,HIGH);
+    Serial.println("bladeStoped");
+    bladeStatusON = false;
+    wavPlay("bldstop.wav",false,"ใบตัดหยุดทำงาน"); //ใบตัดหยุดทำงาน 
+  }      
+
+  ch_1 = readChannel(0, -60, 60, 0);
+  ch_2 = readChannel(1, -60, 60, 0);
+
+  if(ch_1 >= 0 && ch_2 >= 0) Forward(ch_1,ch_2); //เดินหน้า
+  if(ch_1 < 0 && ch_1 >= -30 && ch_1*(-1) <= ch_2) LRotate(ch_1*(-1),ch_2); //หมุนซ้าย
+  if(ch_2 < 0 && ch_2 >= -30 && ch_2*(-1) <= ch_1) RRotate(ch_1,ch_2*(-1)); //หมุนขวา
+
+  if(ch_1 < 0 && ch_2 < 0) Backward(ch_2*(-1),ch_1*(-1)); //ถอยหลัง
+  if(ch_1 < 0 && ch_2 < 30 && ch_1*(-1) > ch_2) LBackward(ch_2,ch_1*(-1)); //ถอยหลังซ้าย  
+  if(ch_2 < 0 && ch_1 < 30 && ch_2*(-1) > ch_1) /RBackward(ch_2*(-1),ch_1); //ถอยหลังขวา 
+    
+  if (ss.available() > 0){
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()){
+      logGPS(gps);
+    }
   }
+
+  delay(250);
 }
+
+void printReport(type,L,R){
+  String strReport = String(type) + " " + String(L_Value) + ":" + String(R_Value);
+  Serial.print(strReport.c_str());
+}
+
+void Foreward(L,R){
+  digitalWrite(rev_L,HIGH);
+  digitalWrite(rev_R,HIGH);
+  int L_Value = map(L,0,60,75,130);
+  int R_Value = map(R,0,60,75,130);
+  analogWrite(pin_L,L_Value);
+  analogWrite(pin_R,R_Value);
+  printReport("Foreward",L_Value,R_Value);
+}
+void LRotate(L,R){
+  digitalWrite(rev_L,LOW);
+  digitalWrite(rev_R,HIGH);
+  int L_Value = map(L,0,60,75,130);
+  int R_Value = map(R,0,60,75,130);
+  analogWrite(pin_L,L_Value);
+  analogWrite(pin_R,R_Value);
+  printReport("Left Rotate",L_Value,R_Value);
+}
+void RRotate(L,R){
+  digitalWrite(rev_L,HIGH);
+  digitalWrite(rev_R,LOW);
+  int L_Value = map(L,0,60,75,130);
+  int R_Value = map(R,0,60,75,130);
+  analogWrite(pin_L,L_Value);
+  analogWrite(pin_R,R_Value);
+  printReport("Right Rotate",L_Value,R_Value);
+}
+void Backward(R,L){
+  digitalWrite(rev_L,LOW);
+  digitalWrite(rev_R,LOW);
+  int L_Value = map(L,0,60,75,130);
+  int R_Value = map(R,0,60,75,130);
+  analogWrite(pin_L,L_Value);
+  analogWrite(pin_R,R_Value);
+  printReport("Backward",L_Value,R_Value);
+}
+void LBackward(R,L){  // function ที่อาจจะต้องคำนวณแรงมอเตอร์ใหม่
+  digitalWrite(rev_L,HIGH);
+  digitalWrite(rev_R,LOW);
+  int L_Value = map(L,0,60,75,130);
+  int R_Value = map(R,0,60,75,130);
+  analogWrite(pin_L,L_Value);
+  analogWrite(pin_R,R_Value);
+  printReport("Left Backward",L_Value,R_Value);
+}
+void RBackward(R,L){  // function ที่อาจจะต้องคำนวณแรงมอเตอร์ใหม่
+  digitalWrite(rev_L,LOW);
+  digitalWrite(rev_R,HIGH);
+  int L_Value = map(L,0,60,75,130);
+  int R_Value = map(R,0,60,75,130);
+  analogWrite(pin_L,L_Value);
+  analogWrite(pin_R,R_Value);
+  printReport("Right Backward",L_Value,R_Value);
+}
+
+void logGPS(gps){
+  // write to sd card;
+}
+
